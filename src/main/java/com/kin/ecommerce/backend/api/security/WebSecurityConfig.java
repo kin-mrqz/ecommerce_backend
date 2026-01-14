82cd19efcff4041722966550316e8df004789b64
kin-mrqz <kinnmrqz@gmail.com>
2026-01-14 19:53:59 +0800

    feat: improved endpoint security

--- src/main/java/com/kin/ecommerce/backend/api/security/WebSecurityConfig.java
+++ src/main/java/com/kin/ecommerce/backend/api/security/WebSecurityConfig.java
@@ -5,14 +5,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @Configuration
 public class WebSecurityConfig {
+
+    private JWTRequestFilter jwtRequestFilter;
+
+    public WebSecurityConfig(JWTRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()
+                .addFilterBefore(jwtRequestFilter, AuthorizationFilter.class)
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers("/product", "/auth/register", "/auth/login").permitAll()
+                                .anyRequest().authenticated()
         );
         return http.build();
     }
